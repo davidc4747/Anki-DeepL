201e9530686fc2d1f2f99f02a9921911b49e6276
David G Chung <davidc4747@yahoo.com>
2025-04-29 12:13:39 -0700

    Remove my util functions, access the field dictionary directly

--- __init__.py
+++ __init__.py
@@ -2,11 +2,10 @@ from aqt import gui_hooks, mw
 from aqt.qt import QAction
 from aqt.operations import QueryOp
 from aqt.operations.note import update_notes
-from anki.notes import Note, NoteId
+from anki.notes import Note
 from .settings.settings_dialog import SettingDialog
 from .config import get_config, restore_defaults, save, set_config_action, UserConfig
 from .deepl import translate_phrases, deepl_usage
-from .utils import get_field_indices
 
 
 # Settings Window
@@ -54,15 +53,16 @@ def generate_missing_fields() -> None:
     notes: list[Note] = []
     for nid in mw.col.find_notes(search):
         note = mw.col.get_note(nid)
-        source_index, target_index = get_field_indices(config, note)
 
-        if source_index != -1:
-            field_len = len(note.values()[source_index])
+        translation_config = config.translations.get(note.note_type().get("name"))
+        if translation_config:
+            field_len = len(note[translation_config.source_field])
             if char_count + field_len < usage.character_limit:
                 notes.append(note)
                 char_count += field_len
 
     # Break it up into multiple translation requests for DeepL
+    print(len(notes), char_count)
     CHUNK_SIZE = 500
     for i in range(0, len(notes), CHUNK_SIZE):
         translate_notes(config, notes[i : i + CHUNK_SIZE])
@@ -71,16 +71,16 @@ def generate_missing_fields() -> None:
 def translate_notes(config: UserConfig, notes: list[Note]) -> None:
     phrases = []
     for note in notes:
-        source_index, target_index = get_field_indices(config, note)
-        if source_index != -1:
-            phrases.append(note.values()[source_index])
+        translation_config = config.translations.get(note.note_type().get("name"))
+        if translation_config:
+            phrases.append(note[translation_config.source_field])
 
     def on_success(english_translations):
         # Update the Note Fields
-        for idx, note in enumerate(notes):
-            source_index, target_index = get_field_indices(config, note)
-            if target_index != -1:
-                note.fields[target_index] = english_translations[idx]
+        for i, note in enumerate(notes):
+            translation_config = config.translations.get(note.note_type().get("name"))
+            if translation_config:
+                note[translation_config.target_field] = english_translations[i]
         update_notes(parent=mw, notes=notes).run_in_background()
 
     # Send the field to DeepL for translation
@@ -90,4 +90,4 @@ def translate_notes(config: UserConfig, notes: list[Note]) -> None:
             config.deepl_auth_key, config.source_lang, config.target_lang, phrases
         ),
         success=on_success,
-    ).with_progress("Tranlating DeepL Fields...").run_in_background()
+    ).with_progress("Tranlating fields with DeepL...").run_in_background()
